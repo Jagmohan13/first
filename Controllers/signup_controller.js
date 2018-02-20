f916a61ab82ea3f81c6333280aaa858781f9b7d2
amrit <amritpalkaur115@gmail.com>
2018-02-20 10:28:18 +0530

    amrit

--- Controllers/signup_controller.js
+++ Controllers/signup_controller.js
@@ -3,7 +3,7 @@ var services = require('../Services/signup_services')
 var async = require('async');
 var bcrypt = require('../lib/bcrypt')
 
-
+// signup
 module.exports=async function signup(req) {
     try {
       
